94ce1cc53f96afb14a657b0ac88c281918233e12
Gerardo Ruiz <grville93@gmail.com>
2024-04-15 16:30:07 -0500

    76. Minimum Window Substring LeetCode Challenge Done

--- LeetCode/leetCode Exercises - Pt. II.py
+++ LeetCode/leetCode Exercises - Pt. II.py
@@ -1806,11 +1806,133 @@ Intuition:
 
 
 
+'76. Minimum Window Substring'
+
+# Input
+
+# # Case 1
+# s, t = 'ADOBECODEBANC', 'ABC'
+# # Output: "BANC"
+
+# # Case 2
+# s, t = 'a', 'a'
+# # Output: "a"
+
+# # Case 3
+# s, t = 'a', 'aa'
+# # Output: "abbbbbcdd"
+
+# # Custom case
+# s, t = 'aaaaaaaaaaaabbbbbcdd', 'abcdd'
+# # Output: "abbbbbcdd"
+
+
+# My approach
+
+# def minWindow(s:str, t:str) -> str:
+
+#     if len(t) > len(s):
+#         return ''
+    
+#     if t == s:
+#         return t
+    
+
+#     for i in range(len(t), len(s) + 1):
+
+#         for j in range((len(s)-i) + 1):
+            
+#             if all([char in s[j:j+i] for char in t]):
+#                 return s[j:j+i]
+            
+#     return ''
+
+'Notes: This solution works up to 57%'
+
+
+# # With an improvement
+# def minWindow(s:str, t:str) -> str:
+
+#     from collections import Counter
+
+#     if len(t) > len(s):
+#         return ''
+    
+#     if t == s:
+#         return t
+    
+#     count_t = Counter(t).items()
+
+#     for i in range(len(t), len(s) + 1):
+
+#         for j in range((len(s)-i) + 1):
+            
+#             subs = s[j:j+i]
+#             count_subs = Counter(subs)
+
+#             if all( (x[0] in count_subs.keys() and x[1] <= count_subs[x[0]]) for x in count_t):
+#                 return s[j:j+i]
+            
+#     return ''
+
+'Notes: This solution works up to 93% and hit the time limit'
+
+
+# Another solution
+
+# def minWindow(s, t):    
+
+#     if not s or not t:
+#         return ""
+
+
+#     from collections import defaultdict
+
+#     dictT = defaultdict(int)
+#     for c in t:
+#         dictT[c] += 1
+
+#     required = len(dictT)
+#     l, r = 0, 0
+#     formed = 0
+
+#     windowCounts = defaultdict(int)
+#     ans = [-1, 0, 0]
+
+#     while r < len(s):
+#         c = s[r]
+#         windowCounts[c] += 1
+
+#         if c in dictT and windowCounts[c] == dictT[c]:
+#             formed += 1
+
+#         while l <= r and formed == required:
+#             c = s[l]
+
+#             if ans[0] == -1 or r - l + 1 < ans[0]:
+#                 ans[0] = r - l + 1
+#                 ans[1] = l
+#                 ans[2] = r
+
+#             windowCounts[c] -= 1
+#             if c in dictT and windowCounts[c] < dictT[c]:
+#                 formed -= 1
+
+#             l += 1
+
+#         r += 1
+
+#     return "" if ans[0] == -1 else s[ans[1]:ans[2] + 1]
+        
+
+# print(minWindow(s,t))
+
+
+
+
+
+
 'xxx'
-
-
-
-
 
 
 
