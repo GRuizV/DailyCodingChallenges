f0ad8b18c6b341eea2f3ce1e04b6507958bdacfc
Gerardo Ruiz <grville93@gmail.com>
2024-06-05 19:07:07 -0500

    150. Evaluate Reverse Polish Notation LeetCode Challenge Done

--- LeetCode/leetCode Exercises - Pt. III.py
+++ LeetCode/leetCode Exercises - Pt. III.py
@@ -2604,19 +2604,99 @@ Revision
 
 
 
-'''149. Max Points on a Line'''
+'''150. Evaluate Reverse Polish Notation'''
+
+# Input
+
+# # Case1
+# tokens = ["2","1","+","3","*"]
+# #Output: 9 / ((2 + 1) * 3)
+
+# # Case2
+# tokens = ["4","13","5","/","+"]
+# #Output: 6 / (4 + (13 / 5) )
+
+# # Case3
+# tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+# #Output: ((((((9 + 3) * -11) / 6) * 10) + 17) + 5)
+
+
+# My approach
+
+'''
+Intuition
+
+    Look from left to right the first operand and operate with the trailing two elements (which should be digits)
+    and return the result to where the first of the digits were and iterate the same process until ther is only
+    3 elements in the list to make the last operation
+
+'''
+
+# def evalPRN(tokens:list[str]) -> int:    
+
+#     operations = [x for x in tokens if x in '+-*/']
+
+#     for _ in range(len(operations)):
+
+#         operation = operations.pop(0)
+#         idx = tokens.index(operation)
+#         return_idx = idx-2
+
+#         operand, num2, num1 = tokens.pop(idx), tokens.pop(idx-1), tokens.pop(idx-2)
+
+#         if operand == '/':
+
+#             num1, num2 = int(num1), int(num2)
+#             op_result = num1//num2 if num2 > 0 else (num1 + (-num1 % num2)) // num2
+
+#         else:
+#             op_result = eval(num1+operand+num2)
+
+#         tokens.insert(return_idx, str(op_result))
+        
+#     return tokens[0]
+
+
+# print(evalPRN(tokens=tokens))
+
+'My solution worked for 81% of the cases'
+
+# # Another Approach
+
+# def evalRPN(tokens):
+        
+#     stack = []
+
+#     for x in tokens:
+
+#         if stack == []:
+#             stack.append(int(x))
+
+#         elif x not in '+-/*':
+#             stack.append(int(x))
+
+#         else:
+
+#             l = len(stack) - 2
+
+#             if x == '+':
+#                 stack[l] = stack[l] + stack.pop()
+
+#             elif x == '-':
+#                 stack[l] = stack[l] - stack.pop()
+
+#             elif x == '*':
+#                 stack[l] = stack[l] * stack.pop()
+
+#             else:
+#                 stack[l] = float(stack[l]) / float(stack.pop())
+#                 stack[l] = int(stack[l])    
+
+#     return stack[0]
+
+# print(evalRPN(tokens=tokens))
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+'''xxx'''
