fce7187a105ffc0dff2d4c888947a0e006b11a04
Gerardo Ruiz <grville93@gmail.com>
2025-02-20 16:51:29 -0500

    LeetCode Challenges 1148. Article Views I and 1683. Invalid Tweets solved

--- LeetCode/SQL/SQL Challenges.sql
+++ LeetCode/SQL/SQL Challenges.sql
@@ -5,6 +5,8 @@ CHALLENGES INDEX
         1757. Recyclable and Low Fat Products
         584. Find Customer Referee
         595. Big Countries
+        1148. Article Views I
+        1683. Invalid Tweets
 
 
 
@@ -76,7 +78,6 @@ CHALLENGES INDEX
     SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y'
 ;
 
-
 -- @block // 584. Find Customer Referee
 
     """
@@ -135,7 +136,6 @@ CHALLENGES INDEX
     SELECT c.name FROM Customer AS c
     WHERE c.referee_id != 2 OR c.referee_id IS NULL
 ;
-
 
 -- @block // 595. Big Countries
 
@@ -199,8 +199,122 @@ CHALLENGES INDEX
     WHERE w.area >= 3000000 OR w.population >= 25000000
 ;
 
+-- @block // 1148. Article Views I
+        
+    """
+    Challenge Statement
+
+        Base
+
+            Table: Views
+                +---------------+---------+
+                | Column Name   | Type    |
+                +---------------+---------+
+                | article_id    | int     |
+                | author_id     | int     |
+                | viewer_id     | int     |
+                | view_date     | date    |
+                +---------------+---------+
+                There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+                Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+                Note that equal author_id and viewer_id indicate the same person.
+        
+
+        Statement
+
+            Write a solution to find all the authors that viewed at least one of their own articles.
+
+            Return the result table sorted by id in ascending order.
+
+            The result format is in the following example.
 
 
+        Example
+
+            Input: 
+            Views table:
+            +------------+-----------+-----------+------------+
+            | article_id | author_id | viewer_id | view_date  |
+            +------------+-----------+-----------+------------+
+            | 1          | 3         | 5         | 2019-08-01 |
+            | 1          | 3         | 6         | 2019-08-02 |
+            | 2          | 7         | 7         | 2019-08-01 |
+            | 2          | 7         | 6         | 2019-08-02 |
+            | 4          | 7         | 1         | 2019-07-22 |
+            | 3          | 4         | 4         | 2019-07-21 |
+            | 3          | 4         | 4         | 2019-07-21 |
+            +------------+-----------+-----------+------------+
+            Output: 
+            +------+
+            | id   |
+            +------+
+            | 4    |
+            | 7    |
+            +------+
+
+    """
+
+    -- Solution
+    SELECT Views.author_id AS id FROM Views
+    WHERE Views.author_id = Views.viewer_id
+    GROUP BY id ORDER BY id ASC
+;
+
+-- @block // 1683. Invalid Tweets
+
+    """
+    Challenge Statement
+
+        Base
+
+            Table: Tweets
+            +----------------+---------+
+            | Column Name    | Type    |
+            +----------------+---------+
+            | tweet_id       | int     |
+            | content        | varchar |
+            +----------------+---------+
+            tweet_id is the primary key (column with unique values) for this table.
+            content consists of characters on an American Keyboard, and no other special characters.
+            This table contains all the tweets in a social media app.
+        
+
+        Statement
+
+            Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+            Return the result table in any order.
+
+            The result format is in the following example.
+
+
+        Example
+
+            Input: 
+            Tweets table:
+            +----------+-----------------------------------+
+            | tweet_id | content                           |
+            +----------+-----------------------------------+
+            | 1        | Let us Code                       |
+            | 2        | More than fifteen chars are here! |
+            +----------+-----------------------------------+
+            Output: 
+            +----------+
+            | tweet_id |
+            +----------+
+            | 2        |
+            +----------+
+            Explanation: 
+            Tweet 1 has length = 11. It is a valid tweet.
+            Tweet 2 has length = 33. It is an invalid tweet.
+
+    """
+
+    -- Solution
+    SELECT Views.author_id AS id FROM Views
+    WHERE Views.author_id = Views.viewer_id
+    GROUP BY id ORDER BY id ASC
+;
 
 
 
