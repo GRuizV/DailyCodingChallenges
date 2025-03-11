b1bb19d36c23d141186728d2eef17844cd3c855b
Gerardo Ruiz <grville93@gmail.com>
2025-03-10 19:20:53 -0500

    181. Employees Earning More Than Their Managers LeetCode Challenge Done

--- LeetCode/SQL/Regular SQL Challenges.sql
+++ LeetCode/SQL/Regular SQL Challenges.sql
@@ -3,7 +3,7 @@ CHALLENGES INDEX
 
     
     176. Second Highest Salary (SUBQUERIES: DISCTINCT, ROW_NUM(), MAX())
-
+    181. Employees Earning More Than Their Managers
 
 '''
 
@@ -132,4 +132,83 @@ CHALLENGES INDEX
         In term of performance it has the 2nd place.
     */
 ;
+
+-- @block // 181. Employees Earning More Than Their Managers
+
+    """
+    Challenge Statement
+
+        Base
+
+            Table: Employee
+            +-------------+---------+
+            | Column Name | Type    |
+            +-------------+---------+
+            | id          | int     |
+            | name        | varchar |
+            | salary      | int     |
+            | managerId   | int     |
+            +-------------+---------+
+            id is the primary key (column with unique values) for this table.
+            Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+        
+
+        Statement
+
+            Write a solution to find the employees who earn more than their managers.
+
+            Return the result table in any order.
+
+
+        Example
+
+            Input: 
+
+            Employee table:
+            +----+-------+--------+-----------+
+            | id | name  | salary | managerId |
+            +----+-------+--------+-----------+
+            | 1  | Joe   | 70000  | 3         |
+            | 2  | Henry | 80000  | 4         |
+            | 3  | Sam   | 60000  | Null      |
+            | 4  | Max   | 90000  | Null      |
+            +----+-------+--------+-----------+
+
+            Output: 
+            +----------+
+            | Employee |
+            +----------+
+            | Joe      |
+            +----------+
+
+            Explanation: Joe is the only employee who earns more than his manager.
+    """
+    
+    SELECT e1.name as Employee
+    FROM Employee e1 JOIN Employee e2
+        ON e1.managerId = e2.id
+    WHERE e1.salary > e2.salary        
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
