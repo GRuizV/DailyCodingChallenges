d5d3c9e9f4065dff1a144033e8179d57f756a0af
Gerardo Ruiz <grville93@gmail.com>
2025-03-17 20:43:13 -0500

    620. Not Boring Movies LeetCode Challenge Done

--- LeetCode/SQL/Regular SQL Challenges.sql
+++ LeetCode/SQL/Regular SQL Challenges.sql
@@ -4,7 +4,7 @@ CHALLENGES INDEX
     *FU: Follow-Up
 
     
-    EASY (9)
+    EASY (10)
     
     181. Employees Earning More Than Their Managers (SELF JOIN)
     182. Duplicate Emails (HAVING)
@@ -15,6 +15,7 @@ CHALLENGES INDEX
     607. Sales Person (LEFT JOIN + SUBQUERY [LEF JOINT + LIKE])
     610. Triangle Judgement (CASE)
     619. Biggest Single Number (MAX() + SUBQUERY)
+    620. Not Boring Movies (CONDITIONALS + ORDER BY)
 
 
     MEDIUM (1)
@@ -705,6 +706,73 @@ CHALLENGES INDEX
         GROUP BY num
         HAVING COUNT(num) = 1
         ) AS mynumber
+;
+
+-- @block // 619. Biggest Single Number
+
+    """
+    Challenge Statement
+
+        Base
+
+            Table: Cinema
+
+            +----------------+----------+
+            | Column Name    | Type     |
+            +----------------+----------+
+            | id             | int      |
+            | movie          | varchar  |
+            | description    | varchar  |
+            | rating         | float    |
+            +----------------+----------+
+            id is the primary key (column with unique values) for this table.
+            Each row contains information about the name of a movie, its genre, and its rating.
+            rating is a 2 decimal places float in the range [0, 10]
+
+                   
+
+        Statement
+
+            Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+            Return the result table ordered by rating in descending order.
+
+            The result format is in the following example.
+
+
+        Example
+
+           Cinema table:
+            +----+------------+-------------+--------+
+            | id | movie      | description | rating |
+            +----+------------+-------------+--------+
+            | 1  | War        | great 3D    | 8.9    |
+            | 2  | Science    | fiction     | 8.5    |
+            | 3  | irish      | boring      | 6.2    |
+            | 4  | Ice song   | Fantacy     | 8.6    |
+            | 5  | House card | Interesting | 9.1    |
+            +----+------------+-------------+--------+
+
+            Output: 
+            +----+------------+-------------+--------+
+            | id | movie      | description | rating |
+            +----+------------+-------------+--------+
+            | 5  | House card | Interesting | 9.1    |
+            | 1  | War        | great 3D    | 8.9    |
+            +----+------------+-------------+--------+
+
+            Explanation: 
+            We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
+
+
+    """
+    
+    -- My Approach
+    SELECT *
+    FROM Cinema 
+    WHERE id % 2 =1
+        AND description NOT LIKE '%boring%'
+    ORDER BY rating DESC
 ;
 
 
