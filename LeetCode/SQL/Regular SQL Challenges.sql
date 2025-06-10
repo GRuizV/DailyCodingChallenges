604b3cac986759a7cd78b52faf968970097937ac
Gerardo Ruiz <grville93@gmail.com>
2025-06-10 14:01:06 -0500

    177. Nth Higest Salary LC Challenge Reviewed

--- LeetCode/SQL/Regular SQL Challenges.sql
+++ LeetCode/SQL/Regular SQL Challenges.sql
@@ -19,9 +19,10 @@ CHALLENGES INDEX
     196. Delete Duplicate Emails (DML [Deletion]: SUBQUERIES & FU: CTE)
 
 
-    MEDIUM (1)
+    MEDIUM (2)
 
     176. Second Highest Salary  (SUBQUERIES: DISCTINCT, ROW_NUM(), MAX())
+    177. Nth Higest Salary  (PL/SQL: Procedural Language SQL)
 
 
 
@@ -980,7 +981,79 @@ CHALLENGES INDEX
     */
 ;
 
+-- @block // 177. Nth Higest Salary
 
+    """
+    Table: Employee
+
+    +-------------+------+
+    | Column Name | Type |
+    +-------------+------+
+    | id          | int  |
+    | salary      | int  |
+    +-------------+------+
+    id is the primary key (column with unique values) for this table.
+    Each row of this table contains information about the salary of an employee.
+    
+
+    Problem Statement
+    Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+    The result format is in the following example.
+
+    
+
+    Example 1:
+
+    Input: 
+    Employee table:
+    +----+--------+
+    | id | salary |
+    +----+--------+
+    | 1  | 100    |
+    | 2  | 200    |
+    | 3  | 300    |
+    +----+--------+
+    n = 2
+    Output: 
+    +------------------------+
+    | getNthHighestSalary(2) |
+    +------------------------+
+    | 200                    |
+    +------------------------+
+    Example 2:
+
+    Input: 
+    Employee table:
+    +----+--------+
+    | id | salary |
+    +----+--------+
+    | 1  | 100    |
+    +----+--------+
+    n = 2
+    Output: 
+    +------------------------+
+    | getNthHighestSalary(2) |
+    +------------------------+
+    | null                   |
+    +------------------------+
+
+    """
+
+    CREATE OR REPLACE FUNCTION NthHighestSalary(N INT)
+    RETURNS TABLE (Salary INT) AS $$
+
+    BEGIN
+        RETURN QUERY
+        SELECT DISTINCT e.salary
+        FROM Employee e
+        ORDER BY e.salary DESC
+        OFFSET N - 1 LIMIT 1;
+    
+    END; 
+    $$ LANGUAGE plpgsql;
+
+-- @block // xx.
 
 
 
