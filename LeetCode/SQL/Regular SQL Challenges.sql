f34501e638729f48db7108c65db935dc0753b245
Gerardo Ruiz <grville93@gmail.com>
2025-06-18 16:38:07 -0500

    1050. Actors and Directors Who Cooperated At Least Three Times LC Challenge Done

--- LeetCode/SQL/Regular SQL Challenges.sql
+++ LeetCode/SQL/Regular SQL Challenges.sql
@@ -4,7 +4,7 @@ CHALLENGES INDEX
     *FU: Follow-Up
 
     
-    EASY (11)
+    EASY (13)
     
     181. Employees Earning More Than Their Managers (SELF JOIN)
     182. Duplicate Emails (HAVING)
@@ -18,6 +18,7 @@ CHALLENGES INDEX
     620. Not Boring Movies (CONDITIONALS + ORDER BY)
     196. Delete Duplicate Emails (DML [Deletion]: SUBQUERIES & FU: CTE)
     627. Swap Salary (DML [Updating]: SET / CASE)
+    1050. Actors and Directors Who Cooperated At Least Three Times (PAIR GROUPING + HAVING)
 
 
     MEDIUM (2)
@@ -928,6 +929,62 @@ CHALLENGES INDEX
     -- END AS swapped_sex
     -- FROM Salary s;
 ;
+
+-- @block // 1050. Actors and Directors Who Cooperated At Least Three Times
+"""
+    Challenge Statement
+
+        Table: ActorDirector
+        +-------------+---------+
+        | Column Name | Type    |
+        +-------------+---------+
+        | actor_id    | int     |
+        | director_id | int     |
+        | timestamp   | int     |
+        +-------------+---------+
+        timestamp is the primary key (column with unique values) for this table.
+        
+
+        Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+        Return the result table in any order.
+        The result format is in the following example.
+
+        
+        Example 1:
+
+        Input: 
+        ActorDirector table:
+        +-------------+-------------+-------------+
+        | actor_id    | director_id | timestamp   |
+        +-------------+-------------+-------------+
+        | 1           | 1           | 0           |
+        | 1           | 1           | 1           |
+        | 1           | 1           | 2           |
+        | 1           | 2           | 3           |
+        | 1           | 2           | 4           |
+        | 2           | 1           | 5           |
+        | 2           | 1           | 6           |
+        +-------------+-------------+-------------+
+
+        Output: 
+        +-------------+-------------+
+        | actor_id    | director_id |
+        +-------------+-------------+
+        | 1           | 1           |
+        +-------------+-------------+
+
+        Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+    """
+    
+    -- DML
+    SELECT actor_id, director_id FROM ActorDirector
+    GROUP BY actor_id, director_id
+    HAVING COUNT(*) > 2;
+;
+
+
+
 
 "MEDIUM"
 
