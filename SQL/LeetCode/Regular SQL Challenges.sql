449c927f11808e44ffe59729533c3695d546e45b
Gerardo Ruiz <grville93@gmail.com>
2025-07-14 11:27:02 -0500

    Changes:
    - Files Renaming.
    
    - LC Challenge '180. Consecutive Numbers' added to 'Regular SQL
      Challenges'.

--- SQL/LeetCode/Regular SQL Challenges.sql
+++ SQL/LeetCode/Regular SQL Challenges.sql
@@ -21,11 +21,12 @@ CHALLENGES INDEX
     1050. Actors and Directors Who Cooperated At Least Three Times (PAIR GROUPING + HAVING)
 
 
-    MEDIUM (3)
+    MEDIUM (4)
 
     176. Second Highest Salary  (SUBQUERIES: DISCTINCT, ROW_NUM(), MAX())
     177. Nth Higest Salary  (PL/SQL: Procedural Language SQL / Type Casting)
     178. Rank Scores. (Window Func -> Dense_rank())
+    180. Consecutive Numbers (SELF JOIN + WHERE conditional)
 
 
 '''
@@ -1256,10 +1257,109 @@ CHALLENGES INDEX
     ORDER BY score DESC--';'
 ;
 
+-- @block // 180. Consecutive Numbers
+
+"""
+    Table: Logs
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | id          | int     |
+    | num         | varchar |
+    +-------------+---------+
+    In SQL, id is the primary key for this table.
+    id is an autoincrement column starting from 1.
+    
+
+    # Problem Statement
+
+    Find all numbers that appear at least three times consecutively.
+    Return the result table in any order.
+    The result format is in the following example.
+
+    
+
+    # Example 1:
+
+    Input: 
+        Logs table:
+        +----+-----+
+        | id | num |
+        +----+-----+
+        | 1  | 1   |
+        | 2  | 1   |
+        | 3  | 1   |
+        | 4  | 2   |
+        | 5  | 1   |
+        | 6  | 2   |
+        | 7  | 2   |
+        +----+-----+
+
+    Output: 
+        +-----------------+
+        | ConsecutiveNums |
+        +-----------------+
+        | 1               |
+        +-----------------+
+    
+    Explanation:
+        1 is the only number that appears consecutively for at least three times.
+
+    """
+
+    SELECT DISTINCT a.num AS ConsecutiveNums
+        FROM Logs a 
+        INNER JOIN Logs b ON a.id = b.id-1
+        INNER JOIN Logs c ON a.id = c.id-2
+    WHERE a.num = b.num AND b.num = c.num --";"
+
+;
 
 
 
 
+
+
+
+"Template"
+-- @block // DDD. ...
+
+"""
+    Table: ...
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | id          | int     |
+    | num         | varchar |
+    +-------------+---------+
+    ...
+    
+
+    # Problem Statement
+
+    ...
+
+    
+
+    # Example 1:
+
+    Input: 
+        ...
+
+    Output: 
+        ...
+    
+    Explanation:
+        ...
+
+    """
+
+    SELECT 
+    --";"
+
+;
 
 
 
